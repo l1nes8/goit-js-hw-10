d145193d237a5564eb61a7a533e4cd4145bca642
l1nes8 <egor1glavatyje@gmail.com>
2023-08-14 19:56:26 +0200

    update

--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -1,83 +1,30 @@
-import Notiflix from 'notiflix';
-import SlimSelect from 'slim-select';
-import 'slim-select/dist/slimselect.css';
-import { createMarkup } from './createMarkup.js';
-import { refs } from './refs.js';
-
+const BASE_URL = 'https://api.thecatapi.com/v1';
 const URL = 'https://api.thecatapi.com/v1/breeds';
 const API_KEY =
   'live_ZRhb9QDqxOnsayJ7SBwmz2cdAH5d3wcwd1nW40wI4YVwoMzYP3fvG0Z6NwvzBO0k';
 
-fetchBreeds().then(render).catch(onError);
-
 export function fetchBreeds() {
-  refs.loader.classList.add('loader-is-visible');
   return fetch(URL, {
     headers: {
       'x-api-key': API_KEY,
     },
   }).then(res => {
-    return res.json();
-  });
-}
-
-function render(data) {
-  data = data.filter(img => img.image?.url != null);
-  const storedBreeds = data;
-
-  for (let i = 0; i < storedBreeds.length; i += 1) {
-    const breed = storedBreeds[i];
-    if (!breed.image) continue;
-
-    let option = document.createElement('option');
-    option.value = breed.id;
-    option.textContent = breed.name;
-    refs.breedSelect.append(option);
-  }
-  selectStyled();
-  refs.loader.classList.remove('loader-is-visible');
-}
-
-function selectStyled() {
-  new SlimSelect({
-    select: refs.breedSelect,
-  });
-}
-
-let page = 1;
-refs.breedSelect.addEventListener('change', onChange);
-
-function onChange(e) {
-  refs.loader.classList.add('loader-is-visible');
-  const option = e.currentTarget;
-  const selectedOption = option.value;
-
-  page += 1;
-
-  fetchCatByBreed(selectedOption)
-    .then(res => {
-      refs.catInfo.innerHTML = createMarkup(res);
-      refs.loader.classList.remove('loader-is-visible');
-    })
-    .catch(onError);
-}
-
-function fetchCatByBreed(breedId) {
-  return fetch(
-    `https://api.thecatapi.com/v1/images/search?page=${page}&breed_ids=${breedId}`,
-    {
-      headers: {
-        'x-api-key': API_KEY,
-      },
+    if (res.ok) {
+      return res.json();
     }
-  ).then(response => {
-    return response.json();
+    throw new Error(res.statusText);
   });
 }
 
-function onError() {
-  refs.loader.classList.remove('loader-is-visible');
-  Notiflix.Notify.failure(
-    'Oops! Something went wrong! Try reloading the page!'
-  );
+export function fetchCatByBreed(breedId, page) {
+  return fetch(`${BASE_URL}/images/search?page=${page}&breed_ids=${breedId}`, {
+    headers: {
+      'x-api-key': API_KEY,
+    },
+  }).then(res => {
+    if (res.ok) {
+      return res.json();
+    }
+    throw new Error(res.statusText);
+  });
 }
--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,57 @@
 import Notiflix from 'notiflix';
 import SlimSelect from 'slim-select';
 import 'slim-select/dist/slimselect.css';
-import { fetchBreeds } from './cat-api.js';
+import { fetchBreeds, fetchCatByBreed } from './cat-api.js';
+import { createMarkup } from './createMarkup.js';
+import { refs } from './refs.js';
 
-fetchBreeds();
+fetchBreeds().then(render).catch(onError);
+
+function render(data) {
+  data = data.filter(img => img.image?.url != null);
+  const storedBreeds = data;
+
+  const options = storedBreeds
+    .filter(breed => breed.image)
+    .map(breed => {
+      let option = document.createElement('option');
+      option.value = breed.id;
+      option.textContent = breed.name;
+      return option;
+    });
+
+  refs.breedSelect.append(...options);
+  selectStyled();
+  refs.loader.classList.remove('loader-is-visible');
+}
+
+function selectStyled() {
+  new SlimSelect({
+    select: refs.breedSelect,
+  });
+}
+
+let page = 1;
+refs.breedSelect.addEventListener('change', onChange);
+
+function onChange(e) {
+  refs.loader.classList.add('loader-is-visible');
+  const option = e.currentTarget;
+  const selectedOption = option.value;
+
+  page += 1;
+
+  fetchCatByBreed(selectedOption, page)
+    .then(res => {
+      refs.catInfo.innerHTML = createMarkup(res);
+      refs.loader.classList.remove('loader-is-visible');
+    })
+    .catch(onError);
+}
+
+function onError() {
+  refs.loader.classList.remove('loader-is-visible');
+  Notiflix.Notify.failure(
+    'Oops! Something went wrong! Try reloading the page!'
+  );
+}
